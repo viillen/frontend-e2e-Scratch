74405c9ac9e5b3410390b989216f71505b058980
maltajohnny <yasmimkalfeltz@gmail.com>
2024-05-08 18:23:50 -0300

    Command cy.session

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,7 @@
-
 describe('Login', () => {
   it('successfully logs in', () => {
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.login()
-    
-    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.guiLogin()
+
     cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
