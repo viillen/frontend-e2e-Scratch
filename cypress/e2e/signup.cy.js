79a9bde00a5fb7bd2b2c3e29bee35440d0c40fda
maltajohnny <yasmimkalfeltz@gmail.com>
2024-05-08 16:56:17 -0300

    Validate fields( Confirmation Code ) signup

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -8,5 +8,7 @@ describe('Sign up', () => {
   it('successfully signs up using confirmation code sent via email', () => {
     cy.fillSignupFormAndSubmit(emailAddress, password)
 
+    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,4 +5,12 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   cy.get('#password').type(password, { log: false })
   cy.get('#confirmPassword').type(password, { log: false })
   cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+    cy.wait('@getNotes')
+  })
 })
